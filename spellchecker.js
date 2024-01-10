83d7b6840a3810aa1cf495abeb7f2179dbbfbaff
cdonohue <chad.donohue@gmail.com>
2024-01-10 15:37:44 -0600

    Make errors more readable

--- spellchecker.js
+++ spellchecker.js
@@ -1,11 +1,12 @@
 #! /usr/bin/env node
 
 import fs from 'fs'
+import chalk from 'chalk'
 
 const dictionaryPath = process.argv[2]
 const textFilePath = process.argv[3]
 
 if (!dictionaryPath || !textFilePath) {
-  console.log('Please provide a dictionary and a text file')
+  console.log(chalk.red('Please provide a dictionary and a text file'))
   process.exit(1)
 }
