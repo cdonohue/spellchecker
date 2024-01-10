e9015c968903b21ef60333b12f446101c8ff9157
cdonohue <chad.donohue@gmail.com>
2024-01-10 15:51:22 -0600

    Add progress indicators for loading dictionary and text file

--- spellchecker.js
+++ spellchecker.js
@@ -2,6 +2,7 @@
 
 import fs from 'fs'
 import chalk from 'chalk'
+import { createSpinner } from 'nanospinner'
 import { createSpellChecker } from './lib/spellingUtils.js'
 
 const dictionaryPath = process.argv[2]
@@ -12,5 +13,21 @@ if (!dictionaryPath || !textFilePath) {
   process.exit(1)
 }
 
+const wait = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms))
+
 const spellChecker = createSpellChecker(dictionaryPath)
+
+const dictionaryProgress = createSpinner(`Loading ${spellChecker.dictionaryWordCount} words from dictionary`)
+dictionaryProgress.start()
+await wait(1000)
+dictionaryProgress.success()
+
+const textFileProgress = createSpinner('Reading text file')
+const text = fs.readFileSync(textFilePath, 'utf8')
+textFileProgress.start()
+await wait(1000)
+textFileProgress.success()
+
+
+
 
