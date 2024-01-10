17f244bc008ade1852ca7fd7199a9fc2a75a4883
cdonohue <chad.donohue@gmail.com>
2024-01-10 15:46:52 -0600

    Create a spellchecker

--- spellchecker.js
+++ spellchecker.js
@@ -2,6 +2,7 @@
 
 import fs from 'fs'
 import chalk from 'chalk'
+import { createSpellChecker } from './lib/spellingUtils.js'
 
 const dictionaryPath = process.argv[2]
 const textFilePath = process.argv[3]
@@ -10,3 +11,6 @@ if (!dictionaryPath || !textFilePath) {
   console.log(chalk.red('Please provide a dictionary and a text file'))
   process.exit(1)
 }
+
+const spellChecker = createSpellChecker(dictionaryPath)
+
